f61b143804404481f6f0324999cd943e3a107457
Jose Carlos <josecarlos.avilaperea@gmail.com>
2024-01-25 23:35:16 -0500

    was updated to test useLayoutEffect hook

--- src/03-examples/Quote.tsx
+++ src/03-examples/Quote.tsx
@@ -1,5 +1,6 @@
 
 import PropTypes from 'prop-types';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 interface IQuote {
     quote: string,
@@ -7,11 +8,25 @@ interface IQuote {
 }
 
 export const Quote = ({ quote, author }: IQuote) => {
+
+    const pRef = useRef();
+    const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+
+    useLayoutEffect(() => {
+        const { height, width } = pRef.current.getBoundingClientRect();
+        setBoxSize({ height, width });
+    }, [quote])
+
     return (
-        <blockquote className="blockquote text-end">
-            <p className="mb-1">{quote}</p>
-            <footer className="blockquote-footer mt-1">{author}</footer>
-        </blockquote>
+        <>
+            <blockquote className="blockquote text-end" style={{ display: 'flex' }}>
+                <p ref={pRef} className="mb-1">{quote}</p>
+                <footer className="blockquote-footer mt-1">{author}</footer>
+            </blockquote>
+            <code>
+                {JSON.stringify(boxSize)}
+            </code>
+        </>
     )
 }
 
