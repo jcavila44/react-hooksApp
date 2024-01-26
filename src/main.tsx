068a28078cfd1a37a59120fdee2e127e6bf19a7a
Jose Carlos <josecarlos.avilaperea@gmail.com>
2024-01-25 23:05:01 -0500

    Was added FocusScreen component to test useRef hook

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,11 @@ import './index.css'
 // import { HooksApp } from './HooksApp';
 // import { CounterApp, CounterWithCustomHook } from './01-useState';
 // import { FormWithCustomHook, SimpleForm } from './02-useEffect';
-import { MultipleCustomHooks } from './03-examples';
+// import { MultipleCustomHooks } from './03-examples';
+import { FocusScreen } from './04-useRef';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </React.StrictMode>,
 )
