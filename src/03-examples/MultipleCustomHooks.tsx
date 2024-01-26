af41d3cf9b07d6bc8da0c2f45d47c6f11d3e9351
Jose Carlos <josecarlos.avilaperea@gmail.com>
2024-01-25 22:12:41 -0500

    was addes the features in the multiplecustomhook

--- src/03-examples/MultipleCustomHooks.tsx
+++ src/03-examples/MultipleCustomHooks.tsx
@@ -2,14 +2,29 @@ import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
 
-    const { data, hasError, isLoading } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
-
-    console.log({ data, hasError, isLoading });
+    const { data, isLoading } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const { author, quote } = !!data && data[0] || { author: '', quote: '' };
 
     return (
         <>
             <h1>Breaking bad quotes</h1>
             <hr />
+
+            {
+                (isLoading === true) ? (
+                    <div className="alert alert-info text-center"> Cargando... </div>
+                ) : (
+                    <blockquote className="blockquote text-end">
+                        <p className="mb-1">{quote}</p>
+                        <footer className="blockquote-footer mt-1">{author}</footer>
+                    </blockquote>
+                )
+            }
+
+            <button className="btn btn-primary">
+                Next quote
+            </button>
+
         </>
     )
 }
